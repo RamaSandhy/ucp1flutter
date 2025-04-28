59fa959d616486246fe2b571257c70ed886fec1f
RamaSandhy <rshandy07@gmail.com>
2025-04-28 09:36:35 +0700

    Menambahkan bool, dan tampilan awal dengan Scaffold, SafeArea, dan SingleChildScrollView

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -7,3 +7,16 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final emailController = TextEditingController();
     final passwordController = TextEditingController();
+    bool _obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFAF7F5),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
